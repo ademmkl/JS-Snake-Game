0f06042fc20e5a4cf7eaefba9a1073a449b6d6f1
Adem Kılıç <ademk014689@gmail.com>
2022-03-13 16:43:06 +0300

    Fixed

--- snake.js
+++ snake.js
@@ -22,7 +22,7 @@ class SnakeGame{
     onKeyPress = (event) => {
         if ((event.code == "KeyA" || event.code == "ArrowLeft") && this.velocityX != 1){
             this.velocityX = -1;
-            this.velocityY = 0;
+            this.velocityY = 0; 
         }else if ((event.code == "KeyD" || event.code == "ArrowRight") && this.velocityX != -1){
             this.velocityX = 1;
             this.velocityY = 0;
@@ -43,14 +43,15 @@ class SnakeGame{
     update = () => {
         this.positionX += this.velocityX;
         this.positionY += this.velocityY;
-        this.trail.push({positionX: this.positionX, positionY: this.positionY});
-        
+
+        if (this.trail.length <= this.tailSize){
+            this.trail.push({positionX: this.positionX, positionY: this.positionY});   
+        }
+ 
         if (this.velocityX != 0 || this.velocityY != 0){
             this.trail.shift()
-        }
-
-    }
-
+        }    }
+        
     draw = () => {
         this.context.fillStyle = "#000";
         this.context.fillRect(0,0,this.canvas.width,this.canvas.height);
@@ -65,7 +66,7 @@ class SnakeGame{
         });
 
         this.context.fillStyle = "#f00";
-        this.context.fillRect(this.appleY*this.gridSize,this.appleX*this.gridSize, this.gridSize - 5, this.gridSize - 5);
+        this.context.fillRect(this.appleY*this.gridSize,this.appleX*this.gridSize, this.gridSize - 2, this.gridSize - 2);
     }
 }
 
