09f0b5fb5a9b15579c1df1da12d8c57c21d15e0e
Adem Kılıç <ademk014689@gmail.com>
2022-03-13 19:05:47 +0300

    Upgraded

--- snake.js
+++ snake.js
@@ -6,14 +6,13 @@ class SnakeGame{
     }
 
     init(){
-        this.positionX = this.positionY= 10;
+        this.gridSize = this.tileCount = 20;
+        this.positionX = this.positionY= Math.floor(Math.random()*this.gridSize);
         this.appleX = this.appleY= 5;
         this.tailSize= 5;
         this.trail = [];
         this.velocityX = this.velocityY = 0;
-        this.gridSize = this.tileCount = 20;
-        this.time = setInterval(this.loop, 1000/15);
-
+        this.time = setInterval(this.loop, 1000/10);
         this.context.fillStyle = "#000";
         this.context.fillRect(0,0,this.canvas.width,this.canvas.height);
 
@@ -44,21 +43,33 @@ class SnakeGame{
         this.positionX += this.velocityX;
         this.positionY += this.velocityY;
 
-        if (this.trail.length <= this.tailSize){
+        /*if (this.trail.length <= this.tailSize){
             this.trail.push({positionX: this.positionX, positionY: this.positionY});   
         }
- 
+
         if (this.velocityX != 0 || this.velocityY != 0){
-            this.trail.shift()
-        }    }
-        
+            this.trail.shift();
+        }*/
+
+        this.trail.push({positionX: this.positionX, positionY: this.positionY});
+
+        if (this.trail.length > this.tailSize){
+            this.trail.shift();
+        }
+
+        if (this.trail[this.trail.length - 1].positionX == this.appleX && this.trail[this.trail.length - 1].positionY == this.appleY){
+            this.appleX = this.appleY= Math.floor(Math.random()*this.gridSize);
+            this.tailSize++;
+        }
+    }
+
     draw = () => {
         this.context.fillStyle = "#000";
         this.context.fillRect(0,0,this.canvas.width,this.canvas.height);
 
         this.context.fillStyle = "#fff"
         this.context.font = "20px Times New Roman";
-        this.context.fillText(this.tailSize, 20, 40) 
+        this.context.fillText(this.tailSize - 5, 20, 40) 
 
         this.context.fillStyle = "#33cc33";
         this.trail.forEach((t) => {
