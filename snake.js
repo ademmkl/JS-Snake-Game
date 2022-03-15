0642491c57b39eeae715bdfab1eaf97cc399a9ae
Adem Kılıç <ademk014689@gmail.com>
2022-03-15 20:52:40 +0300

    Finally i have done something!

--- snake.js
+++ snake.js
@@ -7,12 +7,13 @@ class SnakeGame{
 
     init(){
         this.gridSize = this.tileCount = 20;
-        this.positionX = this.positionY= Math.floor(Math.random()*this.gridSize);
-        this.appleX = this.appleY= 5;
+        this.positionX = this.positionY = Math.floor(Math.random()*this.gridSize);
+        this.appleX = this.appleY= 0;
         this.tailSize= 5;
         this.trail = [];
-        this.velocityX = this.velocityY = 0;
-        this.time = setInterval(this.loop, 1000/10);
+        this.velocityX = 0
+        this.velocityY = -1;
+        this.time = setInterval(this.loop, 1000/15);
         this.context.fillStyle = "#000";
         this.context.fillRect(0,0,this.canvas.width,this.canvas.height);
 
@@ -42,24 +43,32 @@ class SnakeGame{
     update = () => {
         this.positionX += this.velocityX;
         this.positionY += this.velocityY;
-
-        /*if (this.trail.length <= this.tailSize){
-            this.trail.push({positionX: this.positionX, positionY: this.positionY});   
+        
+        if (this.positionX < 0){
+            this.positionX += 30;
+        }else if (this.positionY < 0){
+            this.positionY += 30;
+        }else if (this.positionX > 29){
+            this.positionX -= 30;
+        } else if (this.positionY > 29){
+            this.positionY -= 30;
         }
 
-        if (this.velocityX != 0 || this.velocityY != 0){
-            this.trail.shift();
-        }*/
+        this.trail.forEach((t)=>{
+            if (this.positionX === t.positionX && this.positionY === t.positionY){
+                this.reset();
+            }
+        });
+
+        if (this.positionX == this.appleX && this.positionY == this.appleY){
+            this.appleX = this.appleY= Math.floor(Math.random()*this.gridSize);
+            this.tailSize++;
+        }
 
         this.trail.push({positionX: this.positionX, positionY: this.positionY});
 
-        if (this.trail.length > this.tailSize){
+        while (this.trail.length > this.tailSize){
             this.trail.shift();
-        }
-
-        if (this.trail[this.trail.length - 1].positionX == this.appleX && this.trail[this.trail.length - 1].positionY == this.appleY){
-            this.appleX = this.appleY= Math.floor(Math.random()*this.gridSize);
-            this.tailSize++;
         }
     }
 
@@ -78,6 +87,11 @@ class SnakeGame{
 
         this.context.fillStyle = "#f00";
         this.context.fillRect(this.appleY*this.gridSize,this.appleX*this.gridSize, this.gridSize - 2, this.gridSize - 2);
+    }
+
+    reset = () => {
+        clearInterval(this.time);
+        this.init();
     }
 }
 
